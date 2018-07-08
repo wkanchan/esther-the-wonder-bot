1c1be4492b190ddcadbc2e406dcc86002f7ed729
Wittawat Kanchanachat <kntt@fb.com>
2018-07-07 19:23:27 -0700

    call bx API

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-// Imports dependencies and set up http server
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  request = require('request'),
+  app = express().use(bodyParser.json());
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log('Esther is listening'));
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
@@ -16,18 +15,31 @@ app.post('/webhook', (req, res) => {
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
+    let responseMessage;
 
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(entry => {
 
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
-      const webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      const command = entry.messaging[0].message;
+      console.log('I heard ' + command);
+
+      switch (command) {
+        case 'crypto':
+          request('https://bx.in.th/api/', (error, response, body) => {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+          });
+          break;
+        default:
+          responseMessage = "I don't know this command. Oink!";
+      }
     });
 
     // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
+    res.status(200).send(responseMessage);
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
